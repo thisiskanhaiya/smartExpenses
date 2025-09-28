21158ef81ac3d327e948bd6b84b1f2af51eec5dc
Kanhaiya Gawade <69666329+thisiskanhaiya@users.noreply.github.com>
2025-09-28 09:43:20 +0530

    initial commite, project setup done

---
+++ smart-expense/smart-expense/src/main/java/com/example/smart_expense/SmartExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpenseApplication.class, args);
+	}
+
+}
---
+++ smart-expense/smart-expense/src/test/java/com/example/smart_expense/SmartExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
